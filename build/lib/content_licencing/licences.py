07853011574f4393f3b2c4044a5dc29efd19e804
Thomas Uher <thomas.uher@sisol-systems.com>
2019-12-03 13:03:00 +0100

    add licence link

--- build/lib/content_licencing/licences.py
+++ build/lib/content_licencing/licences.py
@@ -9,12 +9,18 @@ LICENCE_VERSION_CHOICES = {
 # default is the lates available version
 LICENCE_DEFAULT_VERSIONS = {}
 
+LICENCE_LOOKUP = {} 
+
+
+
 for choice in settings.CONTENT_LICENCING_LICENCES:
     licence_choice = (
         choice['short_name'], choice['full_name']
     )
 
     LICENCE_CHOICES.append(licence_choice)
+
+    LICENCE_LOOKUP[choice['short_name']] = {}
 
     version_choices = []
     latest_version = None
@@ -28,8 +34,14 @@ for choice in settings.CONTENT_LICENCING_LICENCES:
         )
         version_choices.append(version_choice)
 
+        # add to lookup
+        LICENCE_LOOKUP[choice['short_name']][version] = {
+            'full_name' : choice['full_name'],
+            'link' : link,
+        }
+
     LICENCE_VERSION_CHOICES[choice['short_name']] = version_choices
-    LICENCE_DEFAULT_VERSIONS[choice['short_name']] = latest_version
+    LICENCE_DEFAULT_VERSIONS[choice['short_name']] = latest_version    
 
 
 class ContentLicence:
@@ -42,10 +54,15 @@ class ContentLicence:
         
         self.version = version
 
+        self.link = LICENCE_LOOKUP[self.short_name][self.version]['link']
+
     def as_dict(self):
+        
+        
         dic = {
             'short_name' : self.short_name,
             'version' : self.version,
+            'link' : self.link,
         }
 
         return dic
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = []
 
 setup(
     name='content_licencing',
-    version='0.2.1',
+    version='0.2.3',
     description='Content licencing for django',
     long_description=long_description,
     long_description_content_type='text/markdown',
